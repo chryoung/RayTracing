5d4af46d6a6db74ab31a8c83e6e57852584fb6a6
Ewan Redfield <29620734+chryoung@users.noreply.github.com>
2024-08-28 00:07:27 +0800

    Add lock for incresing _total_id and use concepts for ShapeBuilder

--- src/shape/shapebuilder.cpp
+++ src/shape/shapebuilder.cpp
@@ -6,6 +6,7 @@ namespace RayTracer {
 namespace Shape {
 
 std::uint64_t ShapeBuilder::_total_id = 0;
+std::mutex ShapeBuilder::_mutex;
 
 }  // namespace Shape
 }  // namespace RayTracer
--- src/shape/shapebuilder.h
+++ src/shape/shapebuilder.h
@@ -2,6 +2,8 @@
 #include <cstdint>
 #include <memory>
 #include <utility>
+#include <concepts>
+#include <mutex>
 
 #include "basicshape.h"
 
@@ -9,17 +11,19 @@ namespace RayTracer {
 namespace Shape {
 class ShapeBuilder final {
  public:
+  ShapeBuilder() = delete;
   template <class ShapeType, class... ParameterType>
+    requires std::derived_from<ShapeType, BasicShape>
   static std::shared_ptr<ShapeType> build(ParameterType&&... parameters) {
-    static_assert(std::is_base_of<BasicShape, ShapeType>::value, "Shape must be derived from BasicShape.");
-    std::shared_ptr<ShapeType> s(new ShapeType(_total_id, std::forward<ParameterType&&>(parameters)...));
+    std::lock_guard<std::mutex> lock(_mutex);
+    std::shared_ptr<ShapeType> s(new ShapeType(_total_id, std::forward<ParameterType>(parameters)...));
     _total_id++;
     return s;
   }
 
  private:
-  ShapeBuilder() = default;
   static std::uint64_t _total_id;
+  static std::mutex _mutex;
 };
 }  // namespace Shape
 }  // namespace RayTracer
